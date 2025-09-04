2faa158de34f0d2f902ba33cc0a5473da75a02c1
sammyfduncan <SD192837465@hotmail.co.uk>
2025-09-04 23:21:53 +0100

    Implemented initial signalling and backtesting logic

---
+++ py/signals.py
@@ -0,0 +1,40 @@
+import pandas, numpy
+#Adds trading signals to the DataFrame. 
+
+#Takes analysed df and adds new cols with Z-score and signals
+def create_signals(
+        price_data: pandas.DataFrame
+        ) -> pandas.DataFrame:
+    
+    #define signal thresholds
+    entry_threshold = 2.0
+
+
+    #calculate Z-score:
+    #find rolling mean/std deviation of spread col
+    data_series = price_data['Spread'].rolling(window=60)
+    mean = data_series.mean()
+    std_dev = data_series.std()
+
+    #z-score formuila
+    zscore = (price_data['Spread'] - mean) / std_dev
+
+    #add it to a new col
+    price_data = price_data.assign(ZScore=zscore)
+
+
+    #create position signals
+    #using numpy where to set value
+    #placeholders: -1 == short, 1 == long, 0 == flat 
+
+    price_data['position'] = numpy.where(
+        zscore > entry_threshold,
+        -1, #value if above true
+        numpy.where(
+            zscore < -entry_threshold,
+            1, #value if above true
+            0 #value if false
+        )
+    )
+    
+    return price_data
